64226fe8315bdb8b737dbaa623bad3799d5ff567
wangqi <1942460489@qq.com>
2021-12-30 22:00:28 +0800

    canal job.

---
+++ canal-demo/src/main/java/cn/sliew/demo/canal/handler/DefaultEntryHandler.java
@@ -0,0 +1,30 @@
+package cn.sliew.demo.canal.handler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import top.javatool.canal.client.annotation.CanalTable;
+import top.javatool.canal.client.handler.EntryHandler;
+
+import java.util.Map;
+
+@Slf4j
+@CanalTable(value = "all")
+@Component
+public class DefaultEntryHandler implements EntryHandler<Map<String, String>> {
+
+    @Override
+    public void insert(Map<String, String> map) {
+        log.info("增加 {}", map);
+    }
+
+    @Override
+    public void update(Map<String, String> before, Map<String, String> after) {
+        log.info("修改 before {}", before);
+        log.info("修改 after {}", after);
+    }
+
+    @Override
+    public void delete(Map<String, String> map) {
+        log.info("删除 {}", map);
+    }
+}
