e925211ad9387dbb5760f500c7365bcfa3c8f2a1
wangqi <1942460489@qq.com>
2022-02-10 14:44:10 +0800

    flink: wordcount.

--- demo/wordcount/src/main/scala/wordcount/WordCountJob.scala
+++ demo/wordcount/src/main/scala/wordcount/WordCountJob.scala
@@ -1,5 +1,8 @@
 package wordcount
 
+import java.io.File
+
+import org.apache.flink.api.common.RuntimeExecutionMode
 import org.apache.flink.api.scala._
 import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
 
@@ -7,7 +10,14 @@ object WordCountJob {
 
     def main(args: Array[String]): Unit = {
         val env = StreamExecutionEnvironment.getExecutionEnvironment
-        val source = env.readTextFile("words.txt")
+        env.setParallelism(1)
+        env.setRuntimeMode(RuntimeExecutionMode.BATCH)
+
+        val userDir = System.getProperty("user.dir")
+        val separator = System.getProperty("file.separator");
+        val wordsPath = userDir + separator + "demo/wordcount/src/main/resources"
+
+        val source = env.readTextFile(wordsPath)
         source.flatMap(new WordFlatMapFunction())
             .map(string => Tuple2(string, 1))
             .keyBy(tuple => tuple._1)
