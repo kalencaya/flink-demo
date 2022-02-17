6a915a81b85da7ebebb91b1377cb9e68f5978d65
wangqi <wangqi@xinc818.group>
2022-02-17 15:42:56 +0800

    flink: seatunnel job.

---
+++ seatunnel-demo/src/main/java/cn/sliew/flink/demo/seatunnel/demo/JdbcSourceDemo.java
@@ -0,0 +1,17 @@
+package cn.sliew.flink.demo.seatunnel.demo;
+
+import org.apache.seatunnel.Seatunnel;
+import org.apache.seatunnel.config.command.CommandLineArgs;
+
+import java.net.URL;
+
+import static org.apache.seatunnel.utils.Engine.FLINK;
+
+public class JdbcSourceDemo {
+
+    public static void main(String[] args) throws Exception {
+        URL conf = JdbcSourceDemo.class.getClassLoader().getResource("flink.batch.conf");
+        CommandLineArgs flinkArgs = new CommandLineArgs(conf.getPath(), true);
+        Seatunnel.run(flinkArgs, FLINK);
+    }
+}
