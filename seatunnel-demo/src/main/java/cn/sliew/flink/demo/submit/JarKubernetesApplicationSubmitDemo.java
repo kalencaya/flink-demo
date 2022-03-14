547bd6912ce0843442934619646b39325fc029a6
wangqi <wangqi@xinc818.group>
2022-03-14 17:41:01 +0800

    flink: flink on kubernetes submit.

--- seatunnel-demo/src/main/java/cn/sliew/flink/demo/submit/JarKubernetesApplicationSubmitDemo.java
+++ seatunnel-demo/src/main/java/cn/sliew/flink/demo/submit/JarKubernetesApplicationSubmitDemo.java
@@ -1,8 +1,57 @@
 package cn.sliew.flink.demo.submit;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.flink.client.deployment.ClusterClientFactory;
+import org.apache.flink.client.deployment.ClusterDeploymentException;
+import org.apache.flink.client.deployment.ClusterSpecification;
+import org.apache.flink.client.deployment.DefaultClusterClientServiceLoader;
+import org.apache.flink.client.deployment.application.ApplicationConfiguration;
+import org.apache.flink.client.program.ClusterClient;
+import org.apache.flink.client.program.ClusterClientProvider;
+import org.apache.flink.configuration.*;
+import org.apache.flink.kubernetes.KubernetesClusterDescriptor;
+import org.apache.flink.kubernetes.configuration.KubernetesDeploymentTarget;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.util.Collections;
+
+@Slf4j
 public class JarKubernetesApplicationSubmitDemo {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        Configuration config = Util.loadConfiguration();
+        ClusterClientFactory<String> factory = newClientFactory(config);
+        KubernetesClusterDescriptor clusterDescriptor = createClusterDescriptor(factory, config);
 
+        ClusterSpecification clusterSpecification = Util.createClusterSpecification();
+        config.setLong(JobManagerOptions.TOTAL_PROCESS_MEMORY.key(), MemorySize.ofMebiBytes(4096).getBytes());
+        config.setLong(TaskManagerOptions.TOTAL_PROCESS_MEMORY.key(), MemorySize.ofMebiBytes(4096).getBytes());
+
+        ConfigUtils.encodeCollectionToConfig(config, PipelineOptions.JARS, Collections.singletonList(new File(Util.LOCAL_JAR_FILE_PATH)), Object::toString);
+        ApplicationConfiguration applicationConfiguration = new ApplicationConfiguration(new String[]{}, Util.ENTRY_POINT_CLASS_NAME);
+        ClusterClient<String> clusterClient = createClusterClient(clusterDescriptor, clusterSpecification, applicationConfiguration);
+    }
+
+    private static ClusterClientFactory<String> newClientFactory(Configuration config) {
+        config.setString(DeploymentOptions.TARGET, KubernetesDeploymentTarget.APPLICATION.getName());
+
+        DefaultClusterClientServiceLoader serviceLoader = new DefaultClusterClientServiceLoader();
+        return serviceLoader.getClusterClientFactory(config);
+    }
+
+    private static KubernetesClusterDescriptor createClusterDescriptor(ClusterClientFactory<String> factory, Configuration config) throws MalformedURLException {
+        KubernetesClusterDescriptor clusterDescriptor = (KubernetesClusterDescriptor) factory.createClusterDescriptor(config);
+        return clusterDescriptor;
+    }
+
+    private static ClusterClient<String> createClusterClient(KubernetesClusterDescriptor clusterDescriptor,
+                                                             ClusterSpecification clusterSpecification,
+                                                             ApplicationConfiguration applicationConfiguration) throws ClusterDeploymentException {
+
+        ClusterClientProvider<String> provider = clusterDescriptor.deployApplicationCluster(clusterSpecification, applicationConfiguration);
+        ClusterClient<String> clusterClient = provider.getClusterClient();
+        log.info("deploy application with clusterId: {}", clusterClient.getClusterId());
+        return clusterClient;
     }
 }
